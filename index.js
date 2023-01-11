afe410c9143d95e9cd74051fb382242f68dccf4b
amitderi19 <amitderi780@gmail.com>
2023-01-11 15:47:05 +0200

    new function compact

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-let num1=4, num2=2;
+// let num1=prompt('enter number', 4);
+// let num2=2=prompt('enter number', 2);
+// let operation=prompt('enter operation', "+")
+let num1=4, num2=1, operation="+";
 function calc(num1,num2, operation){
-    if(typeof num1==Number &&typeof num2==Number){
+    if(typeof num1==Number && typeof num2==Number){
         switch(operation){
             case "+":
                 alert(num1+num2);
@@ -24,10 +27,17 @@ function calc(num1,num2, operation){
 else{
     alert("NaN");
     }
-}
-// Bamadash.calc("2", "bob", "+")         
+}    
 
-Bamadash.intersection([1,2,3], [3,'1',6], true);
-Bamadash.intersection([1,2,3], [3,'1',6], false);
-Bamadash.intersection([[1], 2, 3], [3, '1',6], false); 
-Bamadash.intersection([1, 2, 3], [3, '1', 6]) 
+let array=[0, 1, false, 2, '', 3];
+let length=array.length;
+function compact(array){
+    for(let i=0; i<length;i++){
+        if(typeof array[i] == NaN || typeof array[i] == false ||
+            typeof array[i] == 0 || typeof array[i] == undefined ||
+            typeof array[i] == null || typeof array[i] == "" ){
+                delete array[i];
+            }
+    }
+}
+
