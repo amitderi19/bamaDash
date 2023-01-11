2ac9d6e694e1a72273018547fb1655e5076ed14d
amitderi19 <amitderi780@gmail.com>
2023-01-11 16:11:39 +0200

    correct by the index file

--- index.js
+++ index.js
@@ -1,43 +1,59 @@
 // let num1=prompt('enter number', 4);
 // let num2=2=prompt('enter number', 2);
 // let operation=prompt('enter operation', "+")
-let num1=4, num2=1, operation="+";
-function calc(num1,num2, operation){
-    if(typeof num1==Number && typeof num2==Number){
-        switch(operation){
-            case "+":
-                alert(num1+num2);
-                break;
-            case "/":
-                alert(num1/num2);
-                break;
-            case "*":
-                alert(num1*num2);
-                break;
-            case "-":
-                alert(num1-num2);
-                break;
-            case "%":
-                alert(num1%num2);
-                break;
-            default:
-                alert("NaN");
-    }
-}
-else{
-    alert("NaN");
-    }
-}    
 
-let array=[0, 1, false, 2, '', 3];
-let length=array.length;
-function compact(array){
-    for(let i=0; i<length;i++){
-        if(typeof array[i] == NaN || typeof array[i] == false ||
-            typeof array[i] == 0 || typeof array[i] == undefined ||
-            typeof array[i] == null || typeof array[i] == "" ){
-                delete array[i];
-            }
+const Bamadash = {
+    calc(operand1, operand2, operator) {
+        if(typeof operand1==Number && typeof operand2==Number){
+            switch(operator){
+                case "+":
+                    alert(operand1+operand2);
+                    break;
+                case "/":
+                    alert(operand1/operand2);
+                    break;
+                case "*":
+                    alert(operand1*operand2);
+                    break;
+                case "-":
+                    alert(operand1-operand2);
+                    break;
+                case "%":
+                    alert(operand1%operand2);
+                    break;
+                default:
+                    alert("NaN");
+        }
+    }
+    else{
+        alert("NaN");
+        }
+    },
+    compact(array){
+        const length=array.length;
+        for(let i=0; i<length;i++){
+            if(typeof array[i] == NaN || typeof array[i] == false ||
+                typeof array[i] == 0 || typeof array[i] == undefined ||
+                typeof array[i] == null || typeof array[i] == "" ){
+                    delete array[i];
+                }
+        }
+    },
+    intersection(array1,array2, isStrict){
+        throw new Error("Method not implimented.");
+    },
+    filter(array) {
+        throw new Error("Method not implimented.");
+    },
+    size(collection) { 
+        throw new Error("Method not implimented.");
+    },
+    castArray(value) {
+        throw new Error("Method not implimented.");
     }
 }
+
+module.exports = Bamadash;
+
+
 
